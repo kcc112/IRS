14139f4c1086b1752b465aec532a333993160dce
Kamil Celejewski <kamil12283@gmail.com>
2020-08-29 22:58:18 +0200

    Fix action description

--- app/javascript/src/components/app/redux/actions.ts
+++ app/javascript/src/components/app/redux/actions.ts
@@ -4,5 +4,5 @@ export const showModalSuccess = createAction('show modal success');
 export const hideModalSuccess = createAction('hide modal success');
 
 export const apiRequestIncrement = createAction('increment api process success');
-export const apiRequestDecrement = createAction('increment api process success');
+export const apiRequestDecrement = createAction('decrement api process success');
 export const apiRequestError = createAction<Error>('error while communicatting with api');
